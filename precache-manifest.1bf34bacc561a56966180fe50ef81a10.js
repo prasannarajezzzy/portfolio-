daa59bb741dbe3763cd9f7a322c226e8feebd9ec
V.Prasanna <30752161+prasannarajezzzy@users.noreply.github.com>
2023-07-13 15:28:29 -0700

    Updates

---
+++ precache-manifest.1bf34bacc561a56966180fe50ef81a10.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "460427b35b8ebe8ad5149a11dea8041c",
+    "url": "/portfolio-/index.html"
+  },
+  {
+    "revision": "e31ec04ba3bc1030bf55",
+    "url": "/portfolio-/static/css/3.9998556b.chunk.css"
+  },
+  {
+    "revision": "9b1be3c90e5f56ad4039",
+    "url": "/portfolio-/static/css/4.01c87591.chunk.css"
+  },
+  {
+    "revision": "f56bb395bf2553234b0d",
+    "url": "/portfolio-/static/css/main.df4626cb.chunk.css"
+  },
+  {
+    "revision": "44edf8c805aa53da00a8",
+    "url": "/portfolio-/static/js/2.1aa2e407.chunk.js"
+  },
+  {
+    "revision": "e31ec04ba3bc1030bf55",
+    "url": "/portfolio-/static/js/3.d7822b9c.chunk.js"
+  },
+  {
+    "revision": "9b1be3c90e5f56ad4039",
+    "url": "/portfolio-/static/js/4.437520a6.chunk.js"
+  },
+  {
+    "revision": "f56bb395bf2553234b0d",
+    "url": "/portfolio-/static/js/main.89c7f0d2.chunk.js"
+  },
+  {
+    "revision": "b4eda954c850f0dcffe3",
+    "url": "/portfolio-/static/js/runtime-main.9b25f93f.js"
+  },
+  {
+    "revision": "b3e9626aa75e4a19d92e2cf59782e68e",
+    "url": "/portfolio-/static/media/IITM.b3e9626a.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio-/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "9b94499ccea3bd82b24cb210733c4b5e",
+    "url": "/portfolio-/static/media/Pacifico-Regular.9b94499c.ttf"
+  },
+  {
+    "revision": "707f552015e4fa5242c2092de87ed5be",
+    "url": "/portfolio-/static/media/arcana-old.707f5520.png"
+  },
+  {
+    "revision": "5b2e65088d4e14cb940f17c137ca685e",
+    "url": "/portfolio-/static/media/chc.5b2e6508.jpg"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/portfolio-/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "4007e3dfb4b1df6bb60f495fd51dc885",
+    "url": "/portfolio-/static/media/dayswappers.4007e3df.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/portfolio-/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "02cf65956223904d8ba52d8593203fdf",
+    "url": "/portfolio-/static/media/eraswaplife.02cf6595.png"
+  },
+  {
+    "revision": "f2ca7ae684d03a11503fa0b6c8c7af12",
+    "url": "/portfolio-/static/media/hackathon.f2ca7ae6.gif"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/portfolio-/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/portfolio-/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "d0d22cb83b22978dfdd8bb142f5ec50a",
+    "url": "/portfolio-/static/media/research.d0d22cb8.gif"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/portfolio-/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "7f0feeedda08cf7d68593454bc2e82b5",
+    "url": "/portfolio-/static/media/talksCardBackAlt.7f0feeed.svg"
+  }
+]);
